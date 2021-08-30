8d34e1ce6c91e6055bfb51a11233cd4ddce2c7eb
jbetkowska <jadwiga_betkowska@discovery.com>
2021-08-30 19:21:46 +0200

    drugi

--- print.py
+++ print.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+s1 = "America"
+s2 = "Japan"
+firsts1 = s1[0]
+firsts2 = s2[0]
+lasts1 = s1[-1]
+lasts2 = s2[-1]
+mids1 = s1[int(len(s1)/2)]
+mids2 = s2[int(len(s2)/2)]
+
+
+print(firsts1+firsts2+mids1+mids2+lasts1+lasts2)
+
+
+str1 = "Python"
+print(str1[::-1])
